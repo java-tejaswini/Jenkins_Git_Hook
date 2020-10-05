cda376a48c77664799953f5bc8a92e52bbcb1174
tejaswini <tejaswini@abc.com>
2020-10-05 12:54:36 +0530

    with annot

--- Spring_Git_Jenkins_Integration/src/main/java/com/example/demo/SpringGitJenkinsIntegrationApplication.java
+++ Spring_Git_Jenkins_Integration/src/main/java/com/example/demo/SpringGitJenkinsIntegrationApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com")
 public class SpringGitJenkinsIntegrationApplication {
 
 	public static void main(String[] args) {
